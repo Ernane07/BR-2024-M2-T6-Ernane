e50f6f31bf2c11277818378898e5a4d4f4a3d0f5
Ernane07 <144393727+Ernane07@users.noreply.github.com>
2023-09-15 19:03:37 -0300

    BR-2024-M2-T6-ERNANE
    # TAREFA: CRIAR TELA DE RESTART
                #  CRIAR MÉTODO PARA REMOVER REPETIÇÃO DE CÓDIGO PARA ESCREVER TEXTO
    
                # Escrever "Press any key to restart"
                # Escrever o acumulado de death_count
                # Escrever o Score atingido naquela partida
    
                ## RESETAR:
                ##  score
                ##  game_speed

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -1,4 +1,7 @@
+from msilib.schema import SelfReg
+from typing import Self
 import pygame
+
 
 from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
 from dino_runner.components.dinosaur import Dinosaur
@@ -98,7 +101,6 @@ class Game:
             self.screen.blit(ICON, (half_screen_width - 20, half_screen_height - 40))
             # TAREFA: CRIAR TELA DE RESTART
             #  CRIAR MÉTODO PARA REMOVER REPETIÇÃO DE CÓDIGO PARA ESCREVER TEXTO
-            
             # Escrever "Press any key to restart"
             # Escrever o acumulado de death_count
             # Escrever o Score atingido naquela partida
@@ -107,9 +109,39 @@ class Game:
             ##  score
             ##  game_speed
 
+        
+    def show_restart_(self):
+        self.screen.fill((255, 255, 255))
+        half_screen_height = SCREEN_HEIGHT // 2
+        half_screen_height = SCREEN_HEIGHT // 2
+        font = pygame.font.font(FONT_STYLE, 22)
+        text = font.render("press any key to restart", True, (0, 0, 0,) )
+        text_rect = text.get_rect()
+        text_rect.center = (half_screen_width, half_screen_height + 50)
+        self.screen.blit(text, text_rect)
+        score_text = font.render("Score: " + str(self.score), True, (0, 0, 0))
+        score_text_rect = score_text.get_rect()
+        score_text_rect.center = (half_screen_width, half_screen_height + 50)
+        self.screen.blit(score, score_text_rect)
+        death_count_text = font.render("Death count: " + str(self.death_count), True, (0, 0, 0))
+        death_count_text_rect = death_count_text.get_rect()
+        death_count_text_rect.center = (half_screen_width, half_screen_height + 100)
+        self.screen.blit (death_count_text. death_count_text_rect)
         pygame.display.update() # .flip()
-        self.handle_events_on_menu()
+        self.handle_events_on_restart_screen()
+    handle_events_on_restart_screen(self)
 
+    for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.playing = False
+                self.running = False
+            elif event.type == pygame.KEYDOWN:
+                self.restart()
+                self.run()
+                
+        pygame.display.update() # .flip()
+                self.handle_events_on_menu()
+    
     def handle_events_on_menu(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
